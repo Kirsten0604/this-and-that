1deb3e2cc8ede77515458e9e9f785252bd26ab4b
Kirsten <Kirsten>
2016-03-22 21:22:44 -1000

    AAGH

--- ex1/js/index.js
+++ ex1/js/index.js
@@ -3,4 +3,19 @@ window.onload = function () {
   // Make it listen for a Click event
     // this event should log to the console the text inside of the <a> tag
     // the event should only log one word per click.
+
+ // add an event listener to each li element inside of the nav menu.
+ // Make it listen for a Click event
+   // this event should log to the console the text inside of the <a> tag
+   // the event should only log one word per click
+
+  var navi = document.getElementsByClassName('navLink');
+ 
+  for(var i = 0; i<navi.length; i++){
+   console.log(navi[i]);
+   navi[i].addEventListener('click', function(){
+     console.log(this.textContent);  //node.textContent property represents the text content of a node and its descendants
+  });
+  }
+  
 };
